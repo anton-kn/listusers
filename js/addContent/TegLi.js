b226d6261fec872407abf39946afb5f725c5344b
anton-kn <antonknjzev@yandex.ru>
2020-08-30 15:06:50 +0400

    Прокомментированы некоторые моменты

--- js/addContent/TegLi.js
+++ js/addContent/TegLi.js
@@ -2,7 +2,7 @@
 // принимает путь к фото
 class TegLi {
   constructor(nameClass, pathPicture, objName) {
-    // класс тега
+    // класс тега html
     this.nameClass = nameClass;
     //расположение фото
     this.pathPicture = pathPicture;
@@ -15,15 +15,18 @@ class TegLi {
   _parsingObj(part){
     // возвращаем первую букву в слове большой - Иванов
     if(part == 0){
+      // Состояние
       return this.objName.title[0].toUpperCase()+this.objName.title.slice(1);
     } else if (part == 1){
+      //имя
       return this.objName.first[0].toUpperCase()+this.objName.first.slice(1);
     }else {
+      // фамилия
       return this.objName.last[0].toUpperCase()+this.objName.last.slice(1);
     }
   }
   // метод для добавления структуры, в которую будем записывать
-  // информацию о пользователе
+  // информацию о пользователе начиная со второго
   addTegLi(){
     // html код, который надо добавить на страницу
     const li = `
@@ -41,5 +44,9 @@ class TegLi {
 
     const block = document.querySelector(this.nameClass);
     block.insertAdjacentHTML('beforeEnd', li);
+  }
+  // первый в списке
+  addFirstLi(){
+    const firstLi = document.querySelector()
   }
 }
